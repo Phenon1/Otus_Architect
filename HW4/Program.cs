bd1aba78f32d997b68aa04e8763a7b7609d89d7b
softline\konovalovvd <Vadim.Konovalov@softline.com>
2026-01-31 01:26:52 +0300

    Сделал все пункты и тесты

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -1,6 +1,40 @@
-﻿Solve(1, 2, 3);
-
-static double[] Solve(double a, double b, double c)
+﻿public class Program
 {
-    return new double[0];
+    const double defaultE = 0.0000001d;
+    static void Main(string[] args)
+    {
+        var result = Solve(1, 2, 3);
+    }
+
+    public static double[] Solve(double a, double b, double c,double eps = defaultE)
+    {
+        if (Math.Abs(a) < eps)
+            throw new AZeroException(a);
+        
+        if(double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
+            throw new InfinityException();
+
+        double d = b * b - 4 * a * c;
+
+        if (d <0)
+            return new double[0];
+
+        else if (Math.Abs(d) < eps)
+            return new double[2]
+                {-b/(2*a) ,-b/(2*a) };
+
+        else return new double[2]
+                { (-b + Math.Sqrt(d))/2*a ,
+                  (-b - Math.Sqrt(d))/2*a };
+    }
+    
+    public class AZeroException : ArgumentException
+    {
+        public AZeroException(double a) : base($"параметр a = {a} слишком близок к 0") { }
+    }
+
+    public class InfinityException : ArgumentException
+    {
+        public InfinityException() : base($"один из параметров не может быть равен бесконечности") { }
+    }
 }
--- Tests/TestProgramSolve.cs
+++ Tests/TestProgramSolve.cs
@@ -1,18 +1,59 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace Tests
 {
-    public class Tests
+    public class TestsSolve
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Solve_RootEmpty()
         {
+            var result = Program.Solve(1d, 0, 1d);
+            Assert.That(result, Is.Empty);
+        }
+        [Test]
+        public void Solve_TwoRoots()
+        {
+            var result =  Program.Solve(1d, 0, -1d);
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(result, Is.EquivalentTo(new[] { 1d, -1d }));
         }
 
         [Test]
-        public void Test1()
+        public void Solve_OneRootsTwice()
         {
-            
-            Assert.Pass();
+            var result = Program.Solve(1d, 2d, 1d);
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(result, Is.EquivalentTo(new[] { -1d, -1d }));
+
         }
+
+        [Test]
+        public void Solve_ANotZero()
+        {
+            double eps = 0.001d;
+            Assert.Throws<Program.AZeroException>(() => Program.Solve(0, 2d, 1d));
+            Assert.Throws<Program.AZeroException>(() => Program.Solve(eps - 0.000001, 2d, 1d, eps));
+            Assert.Throws<Program.AZeroException>(() => Program.Solve(-eps + 0.000001, 2d, 1d, eps));
+            Assert.DoesNotThrow(() => Program.Solve(eps + 0.000001, 2d, 1d, eps));
+
+        }
+
+        [Test]
+        public void Solve_DAroundZero()
+        {
+            double eps = 0.000001d;
+            var result = Program.Solve(0.0001d, 0, 1d,eps);
+            Assert.That(result, Is.Empty);
+
+        }
+        [Test]
+        public void Solve_InvalidArg()
+        {
+            Assert.Throws<Program.InfinityException>(() => Program.Solve(double.PositiveInfinity, 2d, 1d));
+            Assert.Throws<Program.InfinityException>(() => Program.Solve(1d, double.NegativeInfinity, 1d));
+            Assert.Throws<Program.InfinityException>(() => Program.Solve(1d, double.NegativeInfinity, 1d));
+
+        }
+
     }
 }
